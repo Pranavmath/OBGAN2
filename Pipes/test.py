e9bd441bc162ff88729cd4110d0e2d8e679014e4
Pranavmath <90156261+Pranavmath@users.noreply.github.com>
2023-10-27 11:34:49 -0400

    Update test.py

--- Pipes/test.py
+++ Pipes/test.py
@@ -1,10 +1,13 @@
-from trainpipemodels import *
-import torch.nn as nn
+from Pipes.currlearningmodels.lunggen import LoadLungGenerator
+from Pipes.currlearningmodels.nodulegen import LoadNoduleGenerator
+from Pipes.currlearningmodels.mmdetmodel import LoadCVModel
 import torch
-from data import NoduleData
-from torchvision.transforms import functional as TF
 
-dataset = NoduleData("finalCXRDataset/final.csv", "finalCXRDataset/images", 140)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-im = TF.to_pil_image(dataset[2][0])
-im.show()
+# Generators
+lung_generator = LoadLungGenerator(device=device, path="OBGAN2/savedmodels/080000_g.model")
+nodule_generator = LoadNoduleGenerator(device=device, path="OBGAN2/savedmodels/nodulegenerator.pth")
+
+lung_generator.lung_predict(1).save("lung.png")
+nodule_generator.nodule_predict(0.5, 2)[0].save("nodule.pngx")
